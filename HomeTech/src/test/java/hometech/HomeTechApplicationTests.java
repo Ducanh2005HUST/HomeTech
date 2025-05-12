cc926d13e245d659c73197b1cdfb29527323527e
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-05-13 00:21:47 +0700

    create project

---
+++ HomeTech/src/test/java/hometech/HomeTechApplicationTests.java
@@ -0,0 +1,13 @@
+package hometech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
