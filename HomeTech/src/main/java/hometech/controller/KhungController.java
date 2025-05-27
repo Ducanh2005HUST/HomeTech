8c368755d23b2359239a73831d986bf7dda23d00
TaducTG <minhduc2795@gmail.com>
2025-05-27 14:48:01 +0700

    check

---
+++ HomeTech/src/main/java/hometech/controller/KhungController.java
@@ -0,0 +1,38 @@
+package hometech.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+public class KhungController {
+    @FXML
+    private Button buttonCanHo;
+
+    @FXML
+    private Button buttonCuDan;
+
+    @FXML
+    private Button buttonDangXuat;
+
+    @FXML
+    private Button buttonHoSo;
+
+    @FXML
+    private Button buttonKhoanThu;
+
+    @FXML
+    private Button buttonTaiKhoan;
+
+    @FXML
+    private Button buttonTrangChu;
+
+    @FXML
+    private Label labelAccountName;
+
+    @FXML
+    private Label labelScreenName;
+
+    @FXML
+    void canHoPressed(ActionEvent event) {
+
+    }
+}
